15a1e532ccdb62fd44d95c6fd2e429e4ccc07792
Pedro Serey <serey.pedro@gmail.com>
2022-11-19 18:36:30 -0300

    refactor: adicionaVaga com leitura real de vaga

--- app/Service/VagaService.hs
+++ app/Service/VagaService.hs
@@ -1,13 +1,13 @@
 module Service.VagaService ( adicionaVaga ) where
 
-import Util.DatabaseManager ( addLinha, readArquivo, getValor )
-import Model.Vaga ( Vaga(Vaga) )
+import Util.DatabaseManager
+import Model.Vaga as V
 import Control.Monad ()
 import Data.Time.Clock.POSIX ( getPOSIXTime )
 import Prelude hiding (id)
 
-andarDir :: String
-andarDir = "app/db/andar"
+vagas :: String
+vagas = "app/db/vaga"
 
 -- Função que apresenta menu para adicionar vaga e adiciona em arquivo do andar especificado
 adicionaVaga :: IO ()
@@ -20,19 +20,20 @@ adicionaVaga = do
     andarInput <- getLine
 
     num <- proxNumVaga (read andarInput :: Int)
-    let id = num ++ "-" ++ tipoVeiculo ++ "-" ++ andarInput
+    let vId = show num ++ "-" ++ tipoVeiculo ++ "-" ++ andarInput
     now <- round `fmap` getPOSIXTime
 
-    let vagasArq = andarDir ++ andarInput ++ ".txt"
+    let vagasArq = vagas ++ andarInput ++ ".txt"
 
-    let vaga = Vaga False (read num + 1) (read andarInput :: Int) tipoVeiculo now id
+    let vaga = Vaga False (num + 1) (read andarInput :: Int) tipoVeiculo now vId
     addLinha (show vaga) vagasArq
 
 -- Função que retorna o próximo número de vaga achando a última vaga no andar especificado
-proxNumVaga :: Int -> IO String
+proxNumVaga :: Int -> IO Int
 proxNumVaga andar = do
     -- abre arquivo do andar especifico
-    let vagasArq = andarDir ++ show andar ++ ".txt"
-    vagas <- readArquivo vagasArq -- tratar exception de andar não existir
-
-    getValor "numero" $ last vagas -- tratar exception com arquivo vazio OU garantir que se andar existir, vaga 1 existe
+    let vagasArq = vagas ++ show andar ++ ".txt"
+    vagasString <- readArquivo vagasArq -- tratar exception de andar não existir
+    let vagasList = map (read :: String -> Vaga) vagasString
+    let vagasAndar = [s | s <- reverse vagasList, V.andar s == andar]
+    return $ V.numero (head vagasAndar)
