db6cf817be35d80dac9d5397e331c2d2663c49cb
jhenriwue <joao.ramalho@ccc.ufcg.edu.br>
2022-11-22 21:14:16 -0300

    closes#10

--- app/Service/EstacionamentoService.hs
+++ app/Service/EstacionamentoService.hs
@@ -1,14 +1,37 @@
 module Service.EstacionamentoService where
 
+import Control.Monad ()
+import Model.Vaga (tipo, tempoInicial, Vaga())
 import Util.DatabaseManager
-import Model.Vaga
-import Data.Time.Clock.POSIX ( getPOSIXTime )
-import Data.Time.Calendar.WeekDate
 
-taxaPagamento :: String -> Integer -> Float
-taxaPagamento tipo dataInicial
-    |tipo == "carro" = 6 + (((now - dataInicial)*3600000)*1.50)
-    |tipo == "moto" = 4 + (((now - dataInicial)*3600000)*1)
-    |tipo == "van" = 8 + (((now - dataInicial)*3600000)*2)
-    |otherwise = "Tipo não existe"
-    where now <- round `fmap` getPOSIXTime
+taxaPagamento :: Vaga -> Int -> Integer ->  Float
+taxaPagamento vaga diaSemana dataFinal = do
+    if tipo vaga == "carro" then do
+        if ((read (show (dataFinal - tempoInicial vaga)) :: Float) > 7200) then do
+            if diaSemana >= 1 && diaSemana <= 5 then do
+                6 + ((((read (show (dataFinal - tempoInicial vaga)))/3600)-2)*1.5)
+            else
+                8 + ((((read (show (dataFinal - tempoInicial vaga)))/3600)-2)*2)
+        else
+            if diaSemana >= 1 && diaSemana <= 5 then 6 
+            else 8
+
+    else if tipo vaga == "moto" then do
+        if ((read (show(dataFinal - tempoInicial vaga)) :: Float) > 7200) then do
+            if diaSemana >= 1 && diaSemana <= 5 then do
+                4 + ((((read (show (dataFinal - tempoInicial vaga)))/3600)-2)*1)
+            else
+                6 + ((((read (show (dataFinal - tempoInicial vaga)))/3600)-2)*1.5)
+        else
+            if diaSemana >= 1 && diaSemana <= 5 then 4 
+            else 6
+
+    else
+        if ((read (show (dataFinal - tempoInicial vaga)) :: Float) > 7200) then do
+            if diaSemana >= 1 && diaSemana <= 5 then do
+                8 + ((((read (show (dataFinal - tempoInicial vaga)))/3600)-2)*2)
+            else
+                10 + ((((read (show (dataFinal - tempoInicial vaga)))/3600)-2)*2.5)
+        else
+            if diaSemana >= 1 && diaSemana <= 5 then 8 
+            else 10
