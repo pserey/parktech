5b51663e823ce8b856097393010b2831adff18bd
jhenriwue <joao.ramalho@ccc.ufcg.edu.br>
2022-11-26 19:24:52 -0300

    maybe fix

--- app/Service/EstacionamentoService.hs
+++ app/Service/EstacionamentoService.hs
@@ -17,39 +17,39 @@ horas dataFinal dataInicial = do
     let date2 =  round dataInicial
     return (date1 - date2)
 
-taxaPagamento :: Vaga -> Int -> IO Float
-taxaPagamento vaga diaSemana = do
-    x <- date
-    let dataFinal = fromIntegral (x)
+taxaPagamento :: Vaga -> Bool -> IO Float
+taxaPagamento vaga iSDiaSemana = do
+    current <- date
+    let dataFinal = fromIntegral (current)
     let dataInicial = fromIntegral (tempoInicial vaga) 
-    c <- horas dataFinal dataInicial
-    let d = fromIntegral(c)
+    diferenca <- horas dataFinal dataInicial
+    let d = fromIntegral(diferenca)
     if tipo vaga == "carro" then do
         if (dataFinal - dataInicial) > 7200 then do
-            if diaSemana >= 1 && diaSemana <= 5 then do
+            if iSDiaSemana then do
                 return (6 + (((d / 3600)-1)*1.5))
             else
                 return (8 + (((d /3600)-1)*2))
         else
-            if diaSemana >= 1 && diaSemana <= 5 then return 6 
+            if iSDiaSemana then return 6 
             else return 8
 
     else if tipo vaga == "moto" then do
         if (dataFinal - dataInicial)  > 7200 then do
-            if diaSemana >= 1 && diaSemana <= 5 then do
+            if iSDiaSemana then do
                 return (4 + ((d /3600)-1))
             else
                 return (6 + (((d /3600)-1)*1.5))
         else
-            if diaSemana >= 1 && diaSemana <= 5 then return 4 
+            if iSDiaSemana then return 4 
             else return 6
 
     else
         if (dataFinal - dataInicial)  > 7200 then do
-            if diaSemana >= 1 && diaSemana <= 5 then do
+            if iSDiaSemana then do
                 return (8 + (((d/3600)-1)*2))
             else
                 return (10 + (((d /3600)-1)*2.5))
         else
-            if diaSemana >= 1 && diaSemana <= 5 then return 8 
+            if iSDiaSemana then return 8 
             else return 10
