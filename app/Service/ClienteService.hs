066cb0eea7fe51ac1e4ed31ccf7f3c7f8f8c8ec5
pserey <serey.pedro@gmail.com>
2022-11-29 01:37:38 -0300

    add: getClienteByCpf

--- app/Service/ClienteService.hs
+++ app/Service/ClienteService.hs
@@ -4,12 +4,20 @@ import Control.Monad ()
 import Model.Cliente
 import Util.DatabaseManager 
 
-clienteArq :: String
-clienteArq = "app/db/cliente.txt"
+clientesArq :: String
+clientesArq = "app/db/cliente.txt"
 
 verificaCliente :: String -> IO Bool
 verificaCliente cpfCliente = do
-    clientesString <- readArquivo clienteArq
+    clientesString <- readArquivo clientesArq
     let clientes = map (read :: String -> Cliente) clientesString
-    let clienteCPF = [s | s <- clientes, cpf s == cpfCliente]
-    return $ not (null clienteCPF)
+    let clienteCpf = [s | s <- clientes, cpf s == cpfCliente]
+    return $ not (null clienteCpf)
+
+
+getClienteByCpf :: String -> IO Cliente
+getClienteByCpf cpfCliente = do
+    clientesString <- readArquivo clientesArq
+    let clientes = map (read :: String -> Cliente) clientesString
+    let clienteCpf = [s | s <- clientes, cpf s == cpfCliente]
+    return $ head clienteCpf
