d526cefaccdb14246181829749170f544fe42300
pserey <serey.pedro@gmail.com>
2022-11-29 15:14:22 -0300

    add: getHistoricoByCpf em ClienteService

--- app/Service/ClienteService.hs
+++ app/Service/ClienteService.hs
@@ -2,10 +2,14 @@ module Service.ClienteService where
 
 import Control.Monad ()
 import Model.Cliente
+import Model.Historico
 import Util.DatabaseManager 
 
 clientesArq :: String
 clientesArq = "app/db/cliente.txt"
+
+historicosArq :: String
+historicosArq = "app/db/historico.txt"
 
 verificaCliente :: String -> IO Bool
 verificaCliente cpfCliente = do
@@ -21,3 +25,9 @@ getClienteByCpf cpfCliente = do
     let clientes = map (read :: String -> Cliente) clientesString
     let clienteCpf = [s | s <- clientes, cpf s == cpfCliente]
     return $ head clienteCpf
+
+getHistoricoByCpf :: String -> IO [Historico]
+getHistoricoByCpf cpfCliente = do
+    historicosString <- readArquivo historicosArq
+    let historicos = map (read :: String -> Historico) historicosString
+    return [s | s <- historicos, clienteCpf s == cpfCliente]
